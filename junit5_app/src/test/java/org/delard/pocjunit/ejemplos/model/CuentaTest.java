135dcfd8394d8db7ad2f77e498d0303595b939b5
David de la Rosa Delgado <drd_id@hotmail.com>
2021-09-19 18:46:59 +0200

    Sec2-15.16-Mensajes de error, @Disabled y @DisplayName

--- junit5_app/src/test/java/org/delard/pocjunit/ejemplos/model/CuentaTest.java
+++ junit5_app/src/test/java/org/delard/pocjunit/ejemplos/model/CuentaTest.java
@@ -3,13 +3,17 @@ package org.delard.pocjunit.ejemplos.model;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.delard.pocjunit.ejemplos.exception.DineroInsuficienteException;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
 class CuentaTest {
 
+    @SuppressWarnings("ExcessiveLambdaUsage")
     @Test
+    @DisplayName("Test nombre de la cuenta")
     void testNombreCuenta() {
 
         Cuenta cuenta = new Cuenta("David",new BigDecimal("2154.124"));
@@ -17,12 +21,14 @@ class CuentaTest {
         String real = cuenta.getPersona();
 
         assertNotNull(real);
-        assertEquals(expected, real);
-
+        assertEquals(expected, real,() -> "Nombre de cuenta incorrecto");
     }
 
     @Test
+    @Disabled("Incluido fail hardcode")
+    @DisplayName("Test saldo de la cuenta")
     void testSaldoCuenta(){
+        fail();
         Cuenta cuenta = new Cuenta("David",new BigDecimal("2154.124"));
         assertNotNull(cuenta.getSaldo());
         assertEquals(2154.124,cuenta.getSaldo().doubleValue());
@@ -31,13 +37,15 @@ class CuentaTest {
     }
 
     @Test
-    void testReferenciaCuenta(){
+    @DisplayName("Test de cuentas iguales en objetos distintos")
+     void testReferenciaCuenta(){
         Cuenta cuenta = new Cuenta("John Doe",new BigDecimal("2154.124"));
         Cuenta cuenta2 = new Cuenta("John Doe",new BigDecimal("2154.124"));
         assertEquals(cuenta,cuenta2);
     }
 
     @Test
+    @DisplayName("Test de debito sobre cuentas")
     void testDebitoCuenta(){
         Cuenta cuenta = new Cuenta("John Doe", new BigDecimal("1000.1234"));
 
@@ -54,6 +62,7 @@ class CuentaTest {
     }
 
     @Test
+    @DisplayName("Test de credito sobre cuentas")
     void testCredito(){
         Cuenta cuenta = new Cuenta("John Doe",new BigDecimal("1000.1234"));
 
@@ -71,6 +80,7 @@ class CuentaTest {
     }
 
     @Test
+    @DisplayName("Test de transferencia entre cuentas")
     void testTransferirDineroCuentas(){
         Cuenta cuenta1 = new Cuenta("John Doe",new BigDecimal("2500.1234"));
         Cuenta cuenta2 = new Cuenta("David RD",new BigDecimal("1000.1234"));
@@ -86,6 +96,7 @@ class CuentaTest {
 
     @SuppressWarnings("OptionalGetWithoutIsPresent")
     @Test
+    @DisplayName("Test de relaciones entre cuentas")
     void testRelacionBancoCuentas(){
         Cuenta cuenta1 = new Cuenta("John Doe",new BigDecimal("2500.1234"));
         Cuenta cuenta2 = new Cuenta("David RD",new BigDecimal("1000.1234"));
@@ -106,20 +117,7 @@ class CuentaTest {
                 () -> assertTrue(banco.getCuentas().stream()
                         .anyMatch(c -> c.getPersona().equals("John Doe")))
                  );
-
-
-
     }
-
-
-
-
-
-
-
-
-
-
 
 
 }
